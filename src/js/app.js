206e718c44b307f336b486da1124f07b93b1093c
titoiriepal <titoiriepal@hotmail.com>
2024-05-20 13:59:51 +0200

    Show and hide the mobile menu

---
+++ src/js/app.js
@@ -0,0 +1,34 @@
+const mobileMenuBtn = document.querySelector('#mobile-menu');
+const cerrarMenuBtn = document.querySelector('#cerrar-menu');
+const sidebar = document.querySelector('.sidebar');
+
+if(mobileMenuBtn){
+    mobileMenuBtn.addEventListener('click', function(){
+        sidebar.classList.add('mostrar');
+        
+    })
+}
+
+if(cerrarMenuBtn){
+    cerrarMenuBtn.addEventListener('click', function(){
+        
+        sidebar.classList.add('ocultar');
+
+        setTimeout(() => {
+            sidebar.classList.remove('mostrar');
+            sidebar.classList.remove('ocultar');
+        }, 500);
+        
+    })
+}
+
+//Eliminar la clase de mostrara en un tamaño de tablet o mayores
+
+
+
+window.addEventListener('resize', function (){
+    const anchoPantalla = document.body.clientWidth;
+    if(anchoPantalla > 768){
+        sidebar.classList.remove('mostrar');
+    }
+})
--- views/templates/sidebar.php
+++ views/templates/sidebar.php
@@ -1,9 +1,25 @@
 <aside class="sidebar">
-    <h2>UpTask</h2>
+    <div class="contenedor-sidebar">
+        <h2>UpTask</h2>
+
+        <div class="cerrar-menu">
+        
+            <svg xmlns="http://www.w3.org/2000/svg" id="cerrar-menu"  viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M18 6l-12 12" />
+                <path d="M6 6l12 12" />
+            </svg>
+        </div>
+    </div>
+    
 
     <nav class="sidebar-nav">
         <a class="<?php echo ($class === 'proyectos') ? 'activo' : ''; ?>" href="/dashboard">Proyectos</a>
         <a class="<?php echo ($class === 'crear') ? 'activo' : ''; ?>" href="/crear-proyecto">Crear Proyectos</a>
         <a class="<?php echo ($class === 'perfil') ? 'activo' : ''; ?>" href="/perfil">Perfil</a>
     </nav>
+
+    <div class="cerrar-sesion-mobile">
+        <a href="/logout" class="cerrar-sesion">Cerrar Sesión</a>
+    </div>
 </aside>
